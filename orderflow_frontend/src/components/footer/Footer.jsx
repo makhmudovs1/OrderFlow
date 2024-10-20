ecd4b4c5cd2ebdd57decae626fcab3af1b8b0ec1
Sheravgan <makhmudov.shb@gmail.com>
2024-10-21 01:41:47 +0500

    [SEYE-5] Add Main page

---
+++ orderflow_frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer" id="id">
+      <div className="container">
+        <h1>OrderFlow</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
